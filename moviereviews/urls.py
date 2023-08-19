f79238d6618001b5b5b46353346e634212a846e7
manager428 <sweetbigdream428@gmail.com>
2023-08-19 15:14:36 +0900

    added news app

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,12 +18,14 @@ from django.urls import path
 from movie import views as movieViews
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
     path('signup/', movieViews.signup, name='signup'),
+    path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+  headline = models.CharField(max_length=200)
+  body = models.TextField()
+  date = models.DateField()
+
+  
